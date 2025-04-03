86a73ad2b6d476e0e6c51523a66ce0d093b19964
Aditya Singh <singhas1418@gmail.com>
2025-04-03 15:02:08 +0530

    adding config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: true, 
-  //   port: 3000, 
-  //   strictPort: true, 
-  //   cors: true,
-  //   allowedHosts:["91f1-2401-4900-1c65-4f4a-b8ef-64f3-bc89-bcfc.ngrok-free.app"],
-  // }
-})
-
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.botwaviation.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
